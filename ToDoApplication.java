7624828120c952970315a8ea3b3b3186f4682aae
sravani131 <84432325+sravani131@users.noreply.github.com>
2025-01-23 12:00:58 +0530

    Create ToDoApplication.java

---
+++ ToDoApplication.java
@@ -0,0 +1,11 @@
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToDoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ToDoApplication.class, args);
+	}
+
+}
